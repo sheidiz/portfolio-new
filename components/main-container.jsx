768501071b37ff44c9370445ea04c63d16eb985d
Sheila Diz <dizsheila07@gmail.com>
2023-12-11 04:45:32 -0300

    style: change main container padding

--- components/main-container.jsx
+++ components/main-container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function MainContainer({children}) {
 	return (
-		<main className="mt-20 md:mt-24 mb-5 mx-auto p-5 min-[320px]:w-11/12 sm:w-[60rem] bg-gray-100 rounded-lg">
+		<main className="mt-20 md:mt-24 mb-5 mx-auto px-10 py-12 min-[320px]:w-11/12 sm:w-[60rem] bg-gray-100 rounded-3xl">
 			{children}
 		</main>
 	)
